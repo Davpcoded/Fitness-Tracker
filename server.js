1aa99b9a2755062e8972504e4e15fe097efc3bb5
Daniel Villarroel-Posada <danielv@friends.local>
2020-08-05 22:17:29 -0700

    initialized server and created DB

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 3000;
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Requiring our routes
+/* require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app); */
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+  useNewUrlParser: true,
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
